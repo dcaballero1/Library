1ec950ed22555a790c8b651d031bfe22fed7f1ee
danel <danel.caballero@aersale.com>
2023-06-05 15:45:56 -0400

    first commit

--- Tutorial/settings.py
+++ Tutorial/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+  
+    
 ]
 
 ROOT_URLCONF = 'Tutorial.urls'
